4d5c2566a7104cf2c432e70978de2c3ef410738a
clement lion <clement.lionceau@gmail.com>
2025-07-20 12:55:47 +0200

    added a level handler which handles level transitions

--- levels/level_handler.lua
+++ levels/level_handler.lua
@@ -1,7 +1,54 @@
--- An object to create a level. A level contains a certain number of ennemies to beat.
+-- An object to initialize all the levels.
 -- When a level ends, the game transitions to the next level.
 
-local LevelHandler = {}
+local Level_1 = require("levels.game_levels.level_1")
+local Level_2 = require("levels.game_levels.level_2")
+local Level_3 = require("levels.game_levels.level_3")
 
+local LevelHandler = {}
+LevelHandler.__index = LevelHandler
+
+function LevelHandler:new(tank_factory, entity_handler, player)
+    local level_handler = {
+        tank_factory = tank_factory,
+        entity_handler = entity_handler,
+        player = player,
+        list_levels = {
+            Level_1,
+            Level_2,
+            Level_3
+        },
+        current_level_index = 1,
+        current_level = nil
+    }
+    setmetatable(level_handler, LevelHandler)
+    level_handler:load_new_level()
+    return level_handler
+end
+
+function LevelHandler:get_player()
+    return self.player
+end
+
+function LevelHandler:load_new_level()
+    if self.current_level_index > #self.list_levels then
+        self.current_level_index = 1
+    end
+    self.current_level = self.list_levels[self.current_level_index].initialize(self.tank_factory, self.entity_handler, self.player)
+end
+
+function LevelHandler:update(dt)
+    self.entity_handler:update(dt)
+    self.current_level:update(dt, self.entity_handler:get_player_position())
+    if self.current_level.finished == true then
+        self.current_level_index = self.current_level_index + 1
+        self:load_new_level()
+    end
+end
+
+function LevelHandler:draw()
+    self.entity_handler:draw()
+    self.current_level:draw_text()
+end
 
 return LevelHandler
--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- Imports of our item
 
 local EntityHandler = require("entities.entity_handler")
-local Level_1 = require("levels.game_levels.level_1")
+local LevelHandler = require("levels.level_handler")
 local PlayerFactory = require("player.player_factory")
 local TankFactory = require("entities.tank.tank_factory")
 
@@ -18,37 +18,32 @@ local CELL_SIZE_Y = 200
 -- Change sizeof screen
 love.window.setMode(1200, 800, flags)
 
--- create out important objects
-local entity_handler = EntityHandler:new(CELL_SIZE_X, CELL_SIZE_Y)
-local player_factory = PlayerFactory:new()
-local tank_factory = TankFactory:new(entity_handler)
-
-local player = player_factory:new_player()
-local level_1 = Level_1.initialize(tank_factory, entity_handler, player)
-
 -- Main methods
 function love.load()
-    
+    local entity_handler = EntityHandler:new(CELL_SIZE_X, CELL_SIZE_Y)
+    local player_factory = PlayerFactory:new()
+    local tank_factory = TankFactory:new(entity_handler)
+
+    local player = player_factory:new_player()
+    LEVEL_HANDLER = LevelHandler:new(tank_factory, entity_handler, player)
 end
 
 function love.update(dt)
-    entity_handler:update(dt)
-    level_1:update(dt, entity_handler:get_player_position())
+    LEVEL_HANDLER:update(dt)
 end
 
 function love.draw()
     -- love.graphics.print("Memory (KB): " .. collectgarbage("count"), 10, 10)
-    entity_handler:draw()
-    level_1:draw_text()
+    LEVEL_HANDLER:draw()
 end
 
 -- Methods to change control type.
 -- If a controller is pressed, we change to detect controller input
 
 function love.keypressed()
-    player.player_controller:set_control_type("keyboard")
+    LEVEL_HANDLER:get_player().player_controller:set_control_type("keyboard")
 end
 
 function love.gamepadpressed(joy)
-    player.player_controller:set_control_type("controller", joy)
+    LEVEL_HANDLER:get_player().player_controller:set_control_type("controller", joy)
 end
